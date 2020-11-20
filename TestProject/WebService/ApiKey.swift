f52dd062654f39f7cc6b1461af7752a419a02ae3
Hemant Soni <hemant.soni@appinventiv.com>
2020-11-20 18:52:27 +0530

    Added a file named ApiKey that contain  API keys and status codes

---
+++ TestProject/WebService/ApiKey.swift
@@ -0,0 +1,20 @@
+//
+//  ApiKey.swift
+//  TestProject
+//
+//  Created by Admin on 20/11/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+enum ApiKey {
+    static var code: String { return "code" }
+    static var message: String { return "message" }
+}
+
+//MARK:- Api Code
+//=======================
+enum ApiCode {
+    static var success: Int { return 200 } // Success
+}
